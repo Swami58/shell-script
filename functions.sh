645b846464ae3789f39cbeda6a0c2177e09fe22e
swami58 <n151302@rguktn.ac.in>
2024-10-30 01:01:48 +0530

    some message

--- functions.sh
+++ functions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPTNAME=$($0 | cut -d "." -f1)
+SCRIPTNAME=$( echo $0 | cut -d "." -f1 )
 LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log
 VALIDATE(){
     if [ $1 -ne 0 ]
