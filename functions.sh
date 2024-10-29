401c8cc6dde60b6f1d7774976542588d7dda14d3
swami58 <n151302@rguktn.ac.in>
2024-10-29 17:11:17 +0530

    some message

--- functions.sh
+++ functions.sh
@@ -1,8 +1,13 @@
 #!/bin/bash 
 VALIDATE(){
+    if [$1 -ne 0 ]
+    then 
+        echo "$2 ... Failure"
+        exit 1
+    else
+        echo "$2 ..Success"
+    fi
 
-echo "Exit status: $1"
-echo "what r you doing"
 }
 
 USERID=$(id -u)
