03d6d59f6cc524be086da74489a3f0d44c23af10
swami <lakamanaswathi2021.com>
2025-02-20 15:41:01 +0530

    some - message

--- 04-variables.sh
+++ 04-variables.sh
@@ -5,3 +5,5 @@ echo "$PERSON1:: Hello $PERSON2, How are you?"
 echo "$PERSON2 :: Hi $PERSON1, I am Fine. How are you?"
 echo "$PERSON1:: I am fine too. how is your work?"
 echo "$PERSON2:: not bad. I am thinking to upgrade to devops" 
+
+#hhssjjhhh
